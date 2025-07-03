0884153aa58043126d023df875a8d9f889b8ae88
Michele Tobias <mmtobias@ucdavis.edu>
2025-07-03 14:13:33 -0700

    script that loads data

---
+++ r/load_data.R
@@ -0,0 +1,11 @@
+# Quarto can't see the variables from previous chapters [commentary withheld] so we need to run a script that will load the data invisibly	
+
+
+
+library(sf)   # for working with spatial data
+
+
+
+monsters <- st_read("data/LakeMonsters.gpkg")
+lakes <- st_read("data/Lakes_GreatLakes-Area.gpkg")
+states <- st_read("data/US_CAN_Admin1.gpkg")
