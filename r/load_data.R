03359fb2373d72dbb637773f1444bc621a0b797b
Michele Tobias <mmtobias@ucdavis.edu>
2025-12-19 09:53:35 -0800

    add raster to intro and base chapters

--- r/load_data.R
+++ r/load_data.R
@@ -3,9 +3,10 @@
 
 
 library(sf)   # for working with spatial data
-
+library(terra)
 
 
 monsters <- st_read("data/LakeMonsters.gpkg")
 lakes <- st_read("data/Lakes_GreatLakes-Area.gpkg")
 states <- st_read("data/US_CAN_Admin1.gpkg")
+dem <- rast("data/dem.tif")
